b29f32b67439fb9f55e607b837ee54e0e8f3a0b9
aadil-nv <adilev2000@gmail.com>
2025-09-29 20:19:05 +0530

    change config env file

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 export interface EnvConfig {
   NODE_ENV: 'development' | 'production' | 'test';
