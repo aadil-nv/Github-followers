fcad0a0f4fbcc7e4691a4a2de094f73d5c331d99
aadil-nv <adilev2000@gmail.com>
2025-09-29 21:42:19 +0530

    added vercel config file

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -1,13 +1,15 @@
 import cors from "cors";
 import { config } from "./env";
 
+// Use a single origin from env
 export const corsOptions: cors.CorsOptions = {
   origin: (origin, callback) => {
-    if (!origin || config.CORS_ORIGINS.includes(origin)) {
+    // allow requests with no origin (like Postman)
+    if (!origin || origin === config.CORS_ORIGINS[0]) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true,
+  credentials: true, // allow cookies or authorization headers
 };
